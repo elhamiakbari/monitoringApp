7bccfe717ec6642fadb045a441a0fbecff727490
vahid-saberi-m <saberi1365@gmial.com>
2023-12-09 17:52:55 +0330

    sql service returns response array

--- service/SqlService.js
+++ service/SqlService.js
@@ -5,105 +5,89 @@ const Connection = require('tedious').Connection;
 const Request = require('tedious').Request;
 
 class SqlService {
-  
-  static async query(dbName, queryPrompt) {
-    return new Promise((resolve, reject) => {
-    var response = [];
-    const config = {
-        server: globalConfig.sql_config[dbName].host,
-        options: {
-            database: globalConfig.sql_config[dbName].database,
-            encrypt: true,
-            trustServerCertificate: true,
-        },
-        authentication: {
-          type: 'default',
-          options: {
-            userName: globalConfig.sql_config[dbName].user,
-            password: globalConfig.sql_config[dbName].password,
-          }
-        }
-    };
-    
-    const connection = new Connection(config);
-    
-    connection.on('connect', (err) => {
-        if (err) {
-            reject(new Error(`POST request failed: ${err.message}`));
-        } 
-        else 
-        {
-            executeStatement().then(res=>{
-                resolve(res);
-            }).catch(error=>{
-                reject(error);
-            })
-        }
-    });
 
-    connection.connect();
-
-    function executeStatement () {
+    static async query(dbName, queryPrompt) {
         return new Promise((resolve, reject) => {
-        var request = new Request(queryPrompt, (err, rowCount) => {
-            if (err) {
-                console.log(err)
-                reject(err)
-            } else {
-                return response;
-            }
-            connection.close()
-        })
-    
-        request.on('row', (columns) => {
-            response = JSON.stringify(columns);
-            resolve(JSON.stringify(columns));
-            // columns.forEach((column) => {
-            //     if (column.value === null) {
-            //         console.log('NULL')
-            //     } else {
-            //         console.log(column.value)
-            //     }
-            // })
-        })
-    
-        connection.execSql(request);
-        })
-}
+            var response = [];
+            const config = {
+                server: globalConfig.sql_config[dbName].host,
+                options: {
+                    database: globalConfig.sql_config[dbName].database,
+                    encrypt: true,
+                    trustServerCertificate: true,
+                },
+                authentication: {
+                    type: 'default',
+                    options: {
+                        userName: globalConfig.sql_config[dbName].user,
+                        password: globalConfig.sql_config[dbName].password,
+                    }
+                }
+            };
 
-    // function createConnection() {
-    //     return sql.createConnection({
-    //         server: globalConfig.sql_config[dbName].host,
-    //         user: globalConfig.sql_config[dbName].user,
-    //         password: globalConfig.sql_config[dbName].password,
-    //         database: config.sql_config[dbName].database,
-    //         port: config.sql_config[dbName].port,
-    //         insecureAuth: true,
-    //         bigNumberStrings: true,
-    //         supportBigNumbers: true,
-    //         connectionLimit: 10,
-    //     })
-    //   };
-      
-    // return new Promise((resolve, reject) => {
-    //     const connection = createConnection();
-    //     connection.connect(err => {
-    //         console.log(err);
-    //     });
-    
-    //     console.log(query);
-    //     connection.query(query,(error, results) => {
-    //       if (error) {
-    //         reject(error);
-    //       } else {
-    //         resolve(results);
-    //       }
-    //     });
-    
-    //     connection.end();
-    // });    
-  }
-)}
+            const connection = new Connection(config);
+
+            connection.on('connect', (err) => {
+                if (err) {
+                    reject(new Error(`POST request failed: ${err.message}`));
+                }
+                else {
+                    executeStatement().then(res => {
+                        resolve(res);
+                    }).catch(error => {
+                        reject(error);
+                    })
+                }
+            });
+
+            connection.connect();
+            function convertToPascalCase(str) {
+                const pascalCase = str.replace(/\w+/g, function (word) {
+                    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+                }).replace(/\s+/g, '');
+
+                return pascalCase.charAt(0).toLowerCase() + pascalCase.slice(1);
+            }
+            function executeStatement() {
+                return new Promise((resolve, reject) => {
+                    var request = new Request(queryPrompt, function(err) {
+                        if (err) {
+                            console.log(err)
+                            reject(err)
+                        } 
+                        // else {
+
+
+                        //     return response;
+                        // }
+                        // connection.close()
+                    })
+
+                    let responseArray = new Array();
+                    request.on('row', (columns) => {
+                        console.log('colu ',columns);
+                        const test = columns.reduce((prevalue, currentValue)=>{
+                            return {...prevalue, ...{[currentValue.metadata.colName]: currentValue.value}}
+                        },{})
+                        responseArray.push(test)                        
+                    })
+                    request.on('done', function(rowCount, more) {  
+                        console.log(rowCount + ' rows returned');  
+                        });  
+                        
+                        // Close the connection after the final event emitted by the request, after the callback passes
+                        request.on("requestCompleted", function (rowCount, more) {
+                            resolve(responseArray);
+                            connection.close();
+                        });
+
+                    connection.execSql(request);
+                })
+            }
+
+        }
+        )
+    }
 }
 
 module.exports = SqlService;
